51ec52ea11bad3e2da5d25a1286c0dfd7b8dfcc1
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2024-05-12 13:42:39 +0300

    RemoveDuplicates

--- src/main/java/problems/P26_Remove_Duplicates_From_Sorted_Array.java
+++ src/main/java/problems/P26_Remove_Duplicates_From_Sorted_Array.java
@@ -5,21 +5,22 @@ import java.util.Arrays;
 public class P26_Remove_Duplicates_From_Sorted_Array {
     public static void main(String[] args) {
 
-        int[] nums = {0,0,1,1,1,2,2,3,3,4};
-        System.out.println( removeDuplicates(nums));
+        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        System.out.println(removeDuplicates(nums));
+
 
     }
 
     public static int removeDuplicates(int[] nums) {
         int k = 0;
         for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[i-1]) {
+            if (nums[i] != nums[i - 1]) {
                 k++;
                 nums[k] = nums[i];
             }
         }
         System.out.println(Arrays.toString(nums));
-        return k+1;
+        return k + 1;
     }
 }
 
